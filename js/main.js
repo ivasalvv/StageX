0619e56138e54b333647a979dac3f6668f55cb75
ivasalvv <ivansaltykov613@gmail.com>
2025-04-16 16:30:20 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -112,3 +112,20 @@ function toggleMenu() {
   burger.classList.toggle('open');
 }
 burger.addEventListener('click', toggleMenu);
+
+// Smooth scroll for an achor link
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute('href');
+    const targetElement = document.querySelector(targetId);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: 'smooth',
+      });
+    }
+  });
+});
